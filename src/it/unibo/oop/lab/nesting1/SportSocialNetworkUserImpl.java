4426c80fdc021b7bc8a35deb71c28a107ee24493
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2021-11-08 12:20:41 +0100

    Add forgotten parentheses in method toString

--- src/it/unibo/oop/lab/nesting1/SportSocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/nesting1/SportSocialNetworkUserImpl.java
@@ -158,12 +158,15 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
     	 */
 		@Override
 		public boolean equals(Object obj) {
-			if (this == obj)
+			if (this == obj) {
 				return true;
-			if (obj == null)
+			}
+			if (obj == null) {
 				return false;
-			if (getClass() != obj.getClass())
+			}
+			if (getClass() != obj.getClass()) {
 				return false;
+			}
 			Sport other = (Sport) obj;
 			return Objects.equals(name, other.name);
 		}
